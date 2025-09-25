9f7dd5ca861bfb48bd1a11d0590bcc96d9a9ceae
norton <3078788052.@qq.com>
2025-09-25 16:23:20 +0800

    重新整合框架

---
+++ src/main/java/com/al/AlApplication.java
@@ -0,0 +1,13 @@
+package com.al;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlApplication.class, args);
+    }
+
+}
